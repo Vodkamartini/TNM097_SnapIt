12fdbb24c96ebab765e9474c907b65902dffba8f
Martin Marklund <martin.marklund@hotmail.se>
2019-03-22 15:34:37 +0100

    Added functionality so it is possible to capture single frame from video stream. Started working on integrating MATLAB into visual studio c++ code.

--- SnapIt/SnapIt/Source.cpp
+++ SnapIt/SnapIt/Source.cpp
@@ -1,18 +1,29 @@
 #include<opencv2/opencv.hpp>
 #include<stdint.h>
+#include<string>
+#include<iostream>
+#include<cmath>
+#include<Engine.h> // Connects to MATLAB
+
+// I have no idea what this does, but it seems to be obligatory
+#pragma comment (lib, "libmat.lib")
+#pragma comment (lib, "libmx.lib")
+#pragma comment (lib, "libmex.lib")
+#pragma comment (lib, "libeng.lib")
+
 
 using namespace cv;
 using namespace std;
 
-const int fps = 60;
+
+
+const int fps = 20;
 
 int main(int argv, char** argc)
 {
-	// Object to keep the video
-	Mat frame;
 
-	// Webcam
-	VideoCapture vid(0);
+	Mat frame;				// Object to keep the video
+	VideoCapture vid(0);	// Webcam
 
 	// If failed to open vid, exit program
 	if (!vid.isOpened())
@@ -23,10 +34,20 @@ int main(int argv, char** argc)
 	while (vid.read(frame))
 	{
 		// Show video feed
-		imshow("Webcam", frame);
+		imshow("Webcam, press Esc to close", frame);
 
-		// If a key is pressed, the application will close
-		if (waitKey(1000 / fps) >= 0)
+
+		// Press s to capture the video frame
+		if (waitKey(1000 / fps) == 's')
+		{
+			Mat frame2;				// Frame for the image we are going to capture
+			frame2 = frame.clone();	// Cloning the image from the live feed
+			imshow("Captured frame", frame2);
+		}
+
+
+		// If Esc key is pressed, the application will close
+		if (waitKey(1000 / fps) == 27)
 			break;
 	}
 
